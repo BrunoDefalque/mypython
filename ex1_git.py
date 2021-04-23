1fb818bd1fab2af971968fc7b85e585f77f16986
Bruno Defalquegit <defalquebruno@gmail.com>
2021-04-23 10:10:20 +0200

    Ajout de la fonction DisplayFirstName

--- ex1_git.py
+++ ex1_git.py
@@ -1,6 +1,9 @@
-name=input("Entre ton prenom : ")
+firstName=input("Entre ton prenom : ")
 
 name=input("Entre ton nom : ")
 def DisplayName(name):
 	print("Votre nom de famille est " + name)
 DisplayName(name)
+def DisplayFirstName(firstName):
+	print("Votre prénom est " + firstName)
+DisplayFirstName(firstName)
